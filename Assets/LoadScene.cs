73c99adbf521ef9c0961311784bf9b26f7fa2a62
LeeDnHe <64310122+LeeDnHe@users.noreply.github.com>
2024-12-10 13:40:11 +0900

    차원문 암스윙

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName = "EndingScene";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
