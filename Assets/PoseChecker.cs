4aa9c337f1eb69532bc6e3db9b48cfde864f03aa
hmud0721@naver.com <hmud0721@naver.com>
2025-01-17 16:30:30 +0900

    포즈 결과 겹치는 부분 이미지로 표현

---
+++ Assets/PoseChecker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseChecker : MonoBehaviour
+{
+    public RenderTexture poseCheckerTexture;
+    public RenderTexture wallCheckerTexture;
+
+    public RenderTexture outputTexture;
+
+    void Update()
+    {
+        
+    }
+}
