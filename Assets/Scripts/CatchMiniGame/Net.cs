4699cee5ed77a9ae817d514c5c970d5d1f73d285
SuKyeong Kim <machine0823@naver.com>
2025-05-31 16:36:58 +0900

    나비잡기 이펙트 사운드 추가 & 거울씬 안내 음성 추가

--- Assets/Scripts/CatchMiniGame/Net.cs
+++ Assets/Scripts/CatchMiniGame/Net.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Net : MonoBehaviour
 {
-    
+    private AudioSource audio;
+    public AudioClip catchSound;
+    public GameObject particle;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         // 나비 태그 확인하고 잡기
@@ -13,6 +22,8 @@ public class Net : MonoBehaviour
             Butterfly butterfly = other.GetComponent<Butterfly>();
             if (butterfly != null)
             {
+                audio.PlayOneShot(catchSound);
+                GameObject particles = Instantiate(particle, other.transform.position, Quaternion.identity);
                 butterfly.Caught();
             }
         }
